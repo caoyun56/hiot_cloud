66514a519c6cde9b469f65cc2ff3e7e9fd57203f
“mgl” <12054628@qq.com>
2021-12-02 13:55:51 +0800

    添加测试日志

--- app/src/main/java/com/example/hiot_cloud/MainActivity.kt
+++ app/src/main/java/com/example/hiot_cloud/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.hiot_cloud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("tag","test")
     }
 }
