6b4ab13615a3108450d41df54c730d3e1dbcfd5b
“mgl” <12054628@qq.com>
2021-12-02 16:05:01 +0800

    添加测试日志

--- app/src/main/java/com/example/hiot_cloud/MainActivity.kt
+++ app/src/main/java/com/example/hiot_cloud/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("tag","test ： 123dqwd")
+        Log.d("tag","test ： 123")
 
     }
 }
